4094c233efc2058b11bb19677d8a22bd8043f579
OhMinSeop <mins5190@gmail.com>
2023-02-05 21:34:42 +0900

    add: 수정

---
+++ src/pages/test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const P = () => {
+  return <p>123</p>;
+};
+
+const test = () => {
+  return (
+    <div>
+      <P />
+      <P />
+    </div>
+  );
+};
+
+export default test;
