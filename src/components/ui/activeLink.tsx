7d207b2ed9bd7c01be5253fd09216c14b8bb26f7
Hangeony <geonhui95@naver.com>
2023-02-15 02:02:51 +0900

    detail page 작업중

---
+++ src/components/ui/activeLink.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import React, { Children } from 'react';
+
+const ActiveLink = ({ children, activeClassName, href, ...props }: any) => {
+  //const { pathname } = useRouter();
+  //const child = Children.only(children);
+  //const childClassName = child.props.className || '';
+
+  //   const className =
+  //     pathname === href
+  //       ? `${childClassName} ${activeClassName}`.trim()
+  //       : childClassName;
+
+  return (
+    <Link href={href} {...props}>
+      {
+        /* {React.cloneElement(child, {
+        className: className || null,
+      })} */
+        children
+      }
+    </Link>
+  );
+};
+
+export default ActiveLink;
--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import Layout from '~/components/shared/Layout/Layout';
+import Divider from '~/components/ui/divider';
+import Container from '~/components/ui/container';
+import Breadcrumb from '~/components/shared/Modal/breadcrumb';
 const Name = () => {
   return (
-    <div>
-      name
-      <input type="text" name="ddd" id="ddd" />
-    </div>
+    <>
+      <Divider className="mb-0" />
+      <Container>
+        <div className="pt-8">
+          <Breadcrumb />
+        </div>
+      </Container>
+    </>
   );
 };
 
 export default Name;
+
+Name.getLayout = function getLayout(page: React.ReactNode) {
+  return <Layout>{page}</Layout>;
+};
