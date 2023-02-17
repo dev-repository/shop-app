b63770e414f71bc33b427b5a6874717cd8101ae3
cheolgyun7 <cjfrbs1256@gmail.com>
2023-02-17 15:48:01 +0900

    230217

--- src/components/my-account/account-layout.tsx
+++ src/components/my-account/account-layout.tsx
@@ -1,14 +1,15 @@
 import Container from "../ui/container";
 import AccountNav from "./account-nav";
 
-const AccountLayout: React.FunctionComponent<{}> = ({ }) => {
+
+const AccountLayout: React.FunctionComponent<{}> = ({ children }) => {
     return (
         <>
             <Container>
                 <div className="py-16 lg:py-20 px-0 xl:max-w-screen-xl mx-auto flex  md:flex-row w-full">
                     <div className="flex flex-col md:flex-row w-full">
-                        {/* <AccountNav /> */}
-                        <div className="md:w-4/6 2xl:w-8/12 mt-4 md:mt-0">{ }</div>
+                        <AccountNav />
+                        <div className="md:w-4/6 2xl:w-8/12 mt-4 md:mt-0">{children}</div>
                     </div>
                 </div>
 
@@ -19,3 +20,7 @@ const AccountLayout: React.FunctionComponent<{}> = ({ }) => {
 }
 
 export default AccountLayout;
+
+
+//children에러
+//accountlayout에 react붙였는데 react안뜸 그래서 index accountlayout에 빨간줄
--- src/pages/my-account/index.tsx
+++ src/pages/my-account/index.tsx
@@ -1,13 +1,33 @@
 import Layout from '~/components/shared/Layout/Layout';
-
+import AccountLayout from '~/components/my-account/account-layout';
 
 
 export default function AccountPage() {
-    return (
-        <>
-            <div>ㅇㅇ</div>
-        </>
-    )
+    return <>
+        {/* 약관제목 및 배경 */}
+        <div
+            className="flex justify-center p-6 md:p-10 2xl:p-8 relative bg-no-repeat bg-center bg-cover"
+            style={{
+                backgroundImage: "url(/assets/images/page-header.jpg)",
+            }}
+        >
+            <div className="absolute top-0 start-0 bg-black w-full h-full opacity-50 transition-opacity duration-500 group-hover:opacity-80" />
+            <div className="w-full flex items-center justify-center relative z-10 py-10 md:py-14 lg:py-20 xl:py-24 2xl:py-32">
+                <h2 className="text-xl md:text-2xl lg:text-3xl font-bold text-white text-center">
+                    <span className="font-satisfy block font-normal mb-3">
+                        explore
+                    </span>
+                    My Account
+                </h2>
+            </div>
+        </div>
+        {/* 약관제목 및 배경 */}
+        <AccountLayout>
+            <h2 className="text-lg md:text-xl xl:text-2xl font-bold text-heading mb-3 xl:mb-5">
+                {("text-dashboard")}
+            </h2>
+        </AccountLayout>
+    </>
 }
 
 AccountPage.getLayout = function getLayout(page: React.ReactNode) {
