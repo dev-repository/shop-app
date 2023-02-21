8161fb4228936b5fbe12e4999e7116e0c04824f9
OhMinSeop <mins5190@gmail.com>
2023-02-21 22:26:35 +0900

    add: trpc 설정 및 사용안하는 모듈 제거

--- src/utils/token.ts
+++
@@ -1,8 +0,0 @@
-import { randomBytes } from 'crypto';
-
-export function generateToken(length = 16): string {
-  return (
-    randomBytes(length).toString('hex') +
-    Math.random().toString(length).substr(2)
-  );
-}
