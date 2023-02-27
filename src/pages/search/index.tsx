c23b263d1f71fde9e6bf689b085213237f4890d7
Hangeony <geonhui95@naver.com>
2023-02-27 22:11:19 +0900

    기타 수정

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from 'next/link';
+import StickyBox from 'react-sticky-box';
+import Layout from '~/components/shared/Layout/Layout';
+import Container from '~/components/ui/container';
+import { BreadcrumbItems } from '~/components/search/Breadcrumb';
+
+export default function Shop() {
+  return (
+    <>
+      <Container>
+        <div className="flex pt-8 pb-16 lg:pb-20">
+          <div className="flex-shrink-0 pe-24 hidden lg:block w-96">
+            <StickyBox offsetTop={50} offsetBottom={20}>
+              <div className="pb-7">
+                <BreadcrumbItems separator="/">
+                  <Link className="text-heading" href={'/'}>
+                    <p>Home</p>
+                  </Link>
+
+                  <Link className="font-semibold text-heading" href={'/search'}>
+                    <p className="capitalize">Search</p>
+                  </Link>
+                </BreadcrumbItems>
+              </div>
+            </StickyBox>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+Shop.getLayout = function getLayout(page: React.ReactNode) {
+  return <Layout>{page}</Layout>;
+};
